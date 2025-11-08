4950916285960aaff494d0dd77c0dfb4d8eaf3d9
benAliAlizadeh <alializadeh.b82@gmail.com>
2025-11-08 18:08:16 +0330

    Add project files.

---
+++ LusterShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LusterShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
